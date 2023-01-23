13f6befc5ca4641777c24f4ee035c5bb511ac48c
paulrdgs <paul.rodrigues@tagproduct.fr>
2023-01-23 15:22:24 +0100

    ajout table skills

--- context/ApplicationDbContext.cs
+++ context/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace ShippeeAPI.Context
         public DbSet<Company> Companies  { get; set; } 
         public DbSet<Naf_Section> Naf_Sections  { get; set; } 
         public DbSet<Naf_Division> Naf_Divisions  { get; set; } 
+        public DbSet<Skill> Skills  { get; set; } 
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
