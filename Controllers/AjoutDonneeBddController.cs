2343dc20948c6e77961d75542bff312458e965c3
paulrdgs <paul.rodrigues@tagproduct.fr>
2023-01-23 19:06:37 +0100

    ajout methode pour ajouter naf_section et naf_division

--- Controllers/AjoutDonneeBddController.cs
+++ Controllers/AjoutDonneeBddController.cs
@@ -53,6 +53,41 @@ public class AjoutDonneeBddController : ControllerBase
 
         return Ok("Les naf_sections sont bien ajouté");
     }
+
+
+
+
+    [HttpGet("Ajout_Naf_Division")]
+    public async Task<IActionResult> GetAjout_Naf_Division()
+    {
+        Workbook wb = new Workbook("G:/Shippee/Naf_Division.xls");
+
+        WorksheetCollection collection = wb.Worksheets;
+        List<Naf_Division> listA = new List<Naf_Division>();
+
+        for (int worksheetIndex = 0; worksheetIndex < collection.Count; worksheetIndex++)
+        {
+            Worksheet worksheet = collection[worksheetIndex];
+
+            int rows = worksheet.Cells.MaxDataRow;
+            int cols = worksheet.Cells.MaxDataColumn;
+
+            for (int i = 0; i <= rows; i++)
+            {
+                Naf_Division naf = new Naf_Division();
+                naf.id = Convert.ToInt32(worksheet.Cells[i, 0].Value);
+                naf.id_naf = Convert.ToInt32(worksheet.Cells[i, 1].Value);
+                naf.title = Convert.ToString(worksheet.Cells[i, 2].Value);
+
+                await _context.Naf_Divisions.AddAsync(naf);
+                await _context.SaveChangesAsync();
+
+                listA.Add(naf);
+            }
+        }
+
+        return Ok("Les naf_divisons sont bien ajouté");
+    }
     
 
     
