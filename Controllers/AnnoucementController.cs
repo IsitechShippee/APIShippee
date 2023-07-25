015cb99e60a031c4e282bd173a7340af45bfcd96
paulrdgs <paul.rodrigues@tagproduct.fr>
2023-07-25 15:14:14 +0200

    methode liste d'annonce selon code postale

--- Controllers/AnnoucementController.cs
+++ Controllers/AnnoucementController.cs
@@ -22,13 +22,94 @@ public class AnnoucementController : ControllerBase
     }
 
     [HttpGet("listannouncement")]
-    public async Task<IActionResult> GetAnnouncement()
+    public async Task<IActionResult> GetAnnouncement(int id, string cp)
     {
-        List<Annoucement> annonce = await _context.Annoucements.Where(i => i.id_status == 2).ToListAsync();
+        List<Annoucement> annonce = new List<Annoucement>();
 
-        var _annonce = _mapper.Map<IEnumerable<AnnoucementStudentDto>>(annonce);
+        if(id == 1)
+        {
+            annonce = await _context.Annoucements.Where(i => i.id_type == 2 || i.id_type == 3).ToListAsync();
+        }
+        else
+        {
+            annonce = await _context.Annoucements.Where(i => i.id_type == 1).ToListAsync();
+        }
 
-        return Ok(_annonce);
+        List<Annoucement> annonce_filter_cp = new List<Annoucement>();
+
+        // ajout des annonces qui ont les memes codes postaux dans une liste
+        foreach(Annoucement annouce in annonce)
+        {
+            if(annouce.id_user != null)
+            {
+                User? user_cp = _context.Users.FirstOrDefault(i => i.id == annouce.id_user);
+                if(user_cp != null)
+                {
+                    if(user_cp.cp != "" && user_cp.cp != null)
+                    {
+                        if(user_cp.cp == cp)
+                        {
+                            annonce_filter_cp.Add(annouce);
+                        }
+                    }
+                    else
+                    {
+                        Company? company_cp = _context.Companies.FirstOrDefault(i => i.siren == user_cp.id_company);
+                        
+                        if(company_cp != null)
+                        {
+                            if(company_cp.cp == cp)
+                            {
+                                annonce_filter_cp.Add(annouce);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // verif si déjà dans le tableau
+        foreach(Annoucement annouce in annonce)
+        {
+            if(annouce.id_user != null)
+            {
+                User? user_cp = _context.Users.FirstOrDefault(i => i.id == annouce.id_user);
+                if(user_cp != null)
+                {
+                    if(user_cp.cp != "" && user_cp.cp != null)
+                    {
+                        if(user_cp.cp.Substring(0, 2) == cp.Substring(0, 2))
+                        {
+                            if(!annonce_filter_cp.Contains(annouce))
+                            {
+                                annonce_filter_cp.Add(annouce);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Company? company_cp = _context.Companies.FirstOrDefault(i => i.siren == user_cp.id_company);
+                        
+                        if(company_cp != null)
+                        {
+                            if(company_cp.cp != null)
+                            {
+                                if(company_cp.cp.Substring(0, 2) == cp.Substring(0, 2))
+                                {
+                                    if(!annonce_filter_cp.Contains(annouce))
+                                    {
+                                        annonce_filter_cp.Add(annouce);
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+        return Ok(annonce_filter_cp);
     }
 
     [HttpPut("UpdateStatusAnnouncement")]
